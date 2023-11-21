f64793c7cb4d026fda039e955d151eaef2ef65f8
Patel-Aum-28 <aumpatel0928@gmail.com>
2023-11-21 23:39:42 +0530

    Update Divide.py

--- Divide.py
+++ Divide.py
@@ -1,13 +1,12 @@
-#How many number are available that can divide provided number
+#Print all numbers from 1 that can divide the entered number.
 
 num = int(input('Enter the number (1 to 1,00,00,000) :- '))
 
-print('List of number that can divide'+ str(num) + ' :- ')
+print(f'List of numbers that can divide {num}:- ')
 
 for i in range(10000000):
     if num == i:
         break
-    
     if i == 0:
         continue
     elif num % i == 0:
