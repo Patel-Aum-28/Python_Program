9a9241b2559d955dd55b55f767879f24a81f638c
Aum Patel <85500430+Patel-Aum-28@users.noreply.github.com>
2023-11-21 23:21:20 +0530

    Update All_prime.py

--- All_prime.py
+++ All_prime.py
@@ -1,8 +1,8 @@
 #Python Program to Print all Prime Numbers in an Interval
 
-ran = int(input("Input the range (Greater than 2):- "))
+interval = int(input("Input the range (Greater than 2):- "))
 
-for num in range(2, ran):
+for num in range(2, interval):
     for devide in range(2, num):
         if num % devide == 0:
             break
