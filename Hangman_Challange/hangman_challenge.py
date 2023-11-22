bafc97a98c3afc6fa1ceabcaf6f9b35289e6e990
Patel-Aum-28 <aumpatel0928@gmail.com>
2023-11-22 16:23:35 +0530

    completed game

--- Hangman_Challange/hangman_challenge.py
+++ Hangman_Challange/hangman_challenge.py
@@ -7,9 +7,9 @@ def starter():
     print("Loading.", end="", flush=True)
     time.sleep(1)
 
-    for _ in range(25):
+    for _ in range(5):
         print(".", end="", flush=True)
-        time.sleep(1)
+        time.sleep(0.5)
 
 def openfile_randomword(file_name):
     with open(file_name, mode="r") as file:
@@ -27,12 +27,39 @@ def empty_list(main_word):
         list_of_letters.append(i)
         list_of_dash.append("_")
     
-    return list_of_letters, list_of_dash
+    return list_of_letters, list_of_dash, length_word
 
+def check_word(user_input, length_word, list_of_letters, list_of_dash):
+    if user_input == "":
+        print("\nPlease enter something!")
+    elif len(user_input) != 1 or user_input.isalpha()==False:
+        print("\nInvalid input. Please enter a single character(A to Z).")
+    else:
+        for position in range(length_word):
+            letter = list_of_letters[position]
+            if letter == user_input:
+                list_of_dash[position] = letter
+
+        if "_" not in list_of_dash:
+            print("\nCongratulations! You guessed the word.")
+            print(f"\nCorrect word is:- {list_of_letters}")
+            return False
+        
+    return True, list_of_dash
 
 path = input("Enter path of wordlist: ")
 main_word = openfile_randomword(path)
-list_of_letters, list_of_dash = empty_list(main_word)
+print("\n")
+starter()
+list_of_letters, list_of_dash, length_word = empty_list(main_word)
 
-print(list_of_letters)
-print(list_of_dash)
+flag = True
+while flag:
+    user_input = input("\nGuess the character:- ").lower()
+    result = check_word(user_input, length_word, list_of_letters, list_of_dash)
+    
+    if isinstance(result, bool):
+        flag = result
+    else:
+        flag, dash = result
+        print(dash)
---
+++ Hangman_Challange/readme.md
@@ -0,0 +1,15 @@
+
+# Hangman Challenge 🎮
+
+Welcome to the Hangman Challenge! This is a simple hangman game written in Python.
+
+## How It Works
+- The game selects a random word from the wordlist.
+- You need to guess the word by entering characters.
+- The game provides list on correct and incorrect guesses.
+- Try to guess the word!
+
+## File Descriptions
+
+- `hangman_challenge.py`: The main Python script for the Hangman game.
+- `hangman_wordlist.txt`: A text file containing a list of words for the game.
